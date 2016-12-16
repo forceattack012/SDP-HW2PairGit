c227add446c45cb14b3992eb401c7e8c0d3f3f18
forceattack012 <wisarutkim012@gmail.com>
2016-12-16 04:39:01 +0000

    First commit

--- printLotto.c
+++ printLotto.c
@@ -5,11 +5,23 @@ int main()
 {
   int i=0,j=0;
   srand(time(NULL));
-  int r[7];
+  int r[7],d[4][3];
   for(i=0;i<6;i++)
   {
       r[i]=rand()%10;
       printf("%d",r[i]);
+  }
+  printf("\n\n");
+  
+ 
+  for(int i=0;i<4;i++)
+  {
+      for(int j=0;j<3;j++)
+      {
+          d[i][j]=rand()%10;
+          printf("%d",d[i][j]);
+      }
+      printf("\n");
   }
 
   return 0;
